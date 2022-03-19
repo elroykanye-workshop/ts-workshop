c109f4aa2c28d07b911722ccf2cf20de6caf5907
Elroy Kanye <elroykanye@gmail.com>
2022-03-19 14:05:03 +0100

    cleared all js files

--- chapter-01/ex02-snippet.ts
+++ chapter-01/ex02-snippet.ts
@@ -2,12 +2,10 @@ function snippet (text: string, length: number): string {
     const ellipsis = "...";
     let result: string = "";
     if (text.length <= length) result = text;
-   else  {
+    else  {
         result = text.slice(0, length - ellipsis.length);
         const lastSpace = result.lastIndexOf(" ");
         result = `${result.slice(0, lastSpace)}${ellipsis}`;
     }
     return result;
 }
-
-snippet(3, 3);
