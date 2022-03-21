c06bd993e61b1d601d35ce6ec41a483229d6bcff
Elroy Kanye <elroykanye@gmail.com>
2022-03-21 09:40:04 +0100

    complete order func using factory pattern

---
+++ chapter-03/ex05-order.ts
@@ -0,0 +1,24 @@
+interface Order {
+    id: number;
+    color: string;
+    size: string;
+}
+
+export const orderFactory = (): ((color: string, size: string, qty: number) => Order[]) => {
+    let id = 0;
+    return (color: string, size: string, qty: number): Order[] => {
+        const orders: Order[] = [];
+        for(let i = 0; i < qty; i++) {
+            orders.push({id: id++, color: color, size: size})
+        }
+        return orders;
+    };
+};
+
+
+const createOrder = orderFactory();
+
+const orderOne = createOrder("red", "M", 4);
+console.log(orderOne);
+const orderTwo = createOrder("blue", "S", 7);
+console.log(orderTwo)
